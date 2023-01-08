a61eddc2ea6ecf795a4a92c4ab37e1d6f9b3fae4
liyuandong <1526566980@qq.com>
2023-01-09 00:40:46 +0800

    Nest_Demo

---
+++ src/user/user.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Request } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) { }
+
+  @Post("info")
+  findUser(@Request() req) {
+    console.log(req.body);
+    return this.userService.findUser();
+  }
+
+}

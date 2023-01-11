b41ef0c9cfe66c51e4a0334fd9cb09fe334b02c7
Mashiro <1526566980@qq.com>
2023-01-11 14:16:15 +0800

    分支

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -5,7 +5,13 @@ export class UserEntity extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column({ type: 'varchar', name: 'user_name' },)
-    name: string;
+    @Column({ type: 'varchar', name: 'user_name', comment: '用户名' })
+    user_name: string;
+
+    @Column({ type: 'varchar', name: 'user_pass', comment: '密码' })
+    user_pass: string;
+
+    @Column({ type: 'varchar', length: 11, name: 'mobile', comment: '手机号' })
+    mobile: string;
 }
 
